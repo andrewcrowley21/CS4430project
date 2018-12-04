227c3f4d877f271e63d916e3f0486f6bf8e7100f
awkemacspjg <45527720+awkemacspjg@users.noreply.github.com>
2018-12-04 16:27:38 -0500

    hopeefully

--- Catalog.php
+++ Catalog.php
@@ -31,6 +31,23 @@
 
 	<h2 align="center">View Our Catalog<h2>
 	
+
+	<?php 
+	//$servername = "sql9.freemysqlhosting.net";
+	//$username = "sql9268464";
+	//$password = "Et55h2EG5e";
+	//$dbname = "sql9268464";
+
+	// Create connection
+	//$conn = pg_connect("host=sql9.freemysqlhosting.net port=3306 dbname=sql9268464 user=sql9268464 password=Et55h2EG5e");
+	// Check connection
+	//if (!$conn) {
+		//echo("Connection failed: We Ded" );
+
+
+
+
+	?>
 	
 </body>
 </html>
