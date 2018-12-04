eaefb426c20af0b060c05e48b2f2e63885f4f716
awkemacspjg <45527720+awkemacspjg@users.noreply.github.com>
2018-12-04 18:17:27 -0500

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -31,37 +31,6 @@
 
 	<h2 align="center">View Our Catalog<h2>
 	
-
-	<?php 
-	//$servername = "sql9.freemysqlhosting.net";
-	//$username = "sql9268464";
-	//$password = "Et55h2EG5e";
-	//$dbname = "sql9268464";
-
-	// Create connection
-	//$conn = pg_connect("host=sql9.freemysqlhosting.net port=3306 dbname=sql9268464 user=sql9268464 password=Et55h2EG5e");
-	// Check connection
-	//if (!$conn) {
-		//echo("Connection failed: We Ded" );
-	//GOTO http://www.phpmyadmin.co AND SIGN IN USING THOSE CREDENTIALS TO ACCESS DATABASE PHPMYADMIN
-	// Use this for learnig how to call, the pg is postgres, not mysql. https://www.w3schools.com/php/php_mysql_intro.asp
-	//class is boring, got a working test file. See you at 6:20ish
-	$con=mysqli_connect("sql9.freemysqlhosting.net","sql9268464","Et55h2EG5e","sql9268464");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-$result = mysqli_query($con,"SELECT * FROM catalog");
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-         echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Price: " . $row["price"]. " - Description: " .  $row["description"] . " - Link:" .  $row["link"] . " Imgpath: ". $row["imgpath"] . " Type:" . $row["type"] . "<br>";
-}}
-$con->close();
-
-
-	?>
 	
 </body>
 </html>
