9929c8cd3e85a13e3302a99f06d39876dfca2e77
awkemacspjg <45527720+awkemacspjg@users.noreply.github.com>
2018-12-06 09:44:13 -0500

    Revert "Update add.php"
    This reverts commit 5ae68f04840348b00a516ea870caac77c601d1fe.

--- add.php
+++ add.php
@@ -3,29 +3,11 @@ include "required.php";
 include "redirect.php";
 
 
+
+
+
 	$IID = $_POST['IID'];
 	$UID = $_POST['UID'];
-	
-	
-	$sql= "SELECT * FROM `user_items_connect`, `user`, `catalog` where catalog.id = user_items_connect.ItemID
-							AND user.UID = user_items_connect.UID and user_items_connect.UID = '" . $UID . "'";
-							
-							$stmt = $pdo->prepare($sql);
-
-
-
-							$stmt->execute();
-
-							$result = $stmt->fetchAll();
-							
-							for($i =0; $i<count($result); $i++){
-								if ($result[$i]['ItemID'] = $IID) {
-									echo '<h3>This item is already in your wish list. Please return to the <a href="catalog.php">catalog page</a> to browse for other options.</h3>';
-								}
-							}
-							
-							
-							
 
 
   $sql = "INSERT INTO user_items_connect (`UID`,`ItemID`) VALUES (?,?)";
