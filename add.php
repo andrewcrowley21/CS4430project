ecbb3a56968b6c53e7f3b758c426e72a3b8a6b69
awkemacspjg <45527720+awkemacspjg@users.noreply.github.com>
2018-12-06 08:49:43 -0500

    Update add.php

--- add.php
+++ add.php
@@ -3,10 +3,6 @@ include "required.php";
 include "redirect.php";
 
 
-
-	echo "Test";
-
-
   $sql = "INSERT INTO user_items_connect (`UID`,`ItemID`) VALUES ($userID,?)";
 
   $stmt = $GLOBALS['pdo']->prepare($sql);
